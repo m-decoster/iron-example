d70d15ee5a0345cddaa886cde874ac8eab6bbd20
Mathieu De Coster <mth.decoster@gmail.com>
2016-11-21 15:41:44 +0100

    Upload

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $.get("http://localhost:3000/feed", function(data) {
+        let feed = "";
+        $.each(data, function() {
+            feed += "<div class=\"post\"><strong>" + this.author_handle + ": </strong>" + this.summary + "</div>";
+        });
+        $("#feed").html(feed);
+    });
+});
+
+$(function() {
+    $('form.postmaker').on('submit', function(e) {
+      e.preventDefault();
+
+      var json = {};
+      form = $(this)[0];
+      $.each(form, function() {
+          if (this.id) {
+              json[this.id] = this.value;
+          }
+      });
+      json.uuid = "ed8729be-f33e-4395-b1c0-f5673668f89e"; // dummy
+      json.message = "";
+      console.log(json);
+
+      $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/post",
+        data: JSON.stringify(json),
+        contentType: 'application/json; charset=utf-8',
+        success: function(msg) {
+            location.reload();
+        },
+        dataType: "json"
+      });
+    });
+});
